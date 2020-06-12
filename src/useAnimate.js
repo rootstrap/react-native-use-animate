59e571fb1305469cc12c172e9b9b1aff5271f9d8
fernandatoledo <fertoledo93@gmail.com>
2020-06-12 14:33:56 -0300

    Add JSDoc to methods

--- src/useAnimate.js
+++ src/useAnimate.js
@@ -1,6 +1,24 @@
 import { useEffect, useRef, useCallback } from 'react';
 import { Animated } from 'react-native';
 
+/**
+ * @function useAnimate
+ * @param {number} fromValue - The value from which the animation will start
+ * @param {number} toValue - The value to which the animation should end
+ * @param {boolean} bounce - True if the animation should return to its initial state
+ * @param {number} iterations - The amount of times the animation should run,
+ * -1 if you want to be an infinite loop
+ * @param {number} duration - The time it takes the value to go from fromValue to toValue
+ *  (or back to its initial state if bounce is true)
+ * @param {string} useNativeDriver - useNativeDriver
+ * @param {boolean} animate - In case this animation will be used for a parallel
+ *  or sequence animation then it should be false
+ * @param {function} callback - The method it should call after the animation is
+ *  ended (in case animate is false then this won't be executed,
+ * it should be passed as part of the Parallel's or sequence's callback)
+ * @param {ref} referenceValue - In case you want to reuse an animation you
+ * can pass its animatedValue as referenceValue
+ */
 const useAnimate = ({
   fromValue = 0,
   toValue = 1,
@@ -10,12 +28,12 @@ const useAnimate = ({
   useNativeDriver = false,
   animate = true,
   callback,
-  initialValue,
+  referenceValue,
 }) => {
   const animatedValue =
-    initialValue || useRef(new Animated.Value(fromValue)).current;
+    referenceValue || useRef(new Animated.Value(fromValue)).current;
   const baseConfig = {
-    duration,
+    duration: bounce ? duration / 2 : duration,
     useNativeDriver,
   };
 
@@ -46,7 +64,7 @@ const useAnimate = ({
   );
 
   const animation =
-    iterations === 1 || callback
+    iterations === 1 || iterations === 0 || callback
       ? sequenceAnimation
       : Animated.loop(sequenceAnimation, { iterations });
 
--- src/useAnimateStructure.js
+++ src/useAnimateStructure.js
@@ -26,6 +26,17 @@ const useAnimateStructure = ({
   return { animation: structuredAnimation, callback };
 };
 
+/**
+ * @function useAnimate
+ * @param {array} animations - The array of animations
+ * @param {number} iterations - The amount of times the animation should run,
+ * -1 if you want to be an infinite loop
+ * @param {boolean} animate - In case this animation will be used for a parallel
+ *  or sequence animation then it should be false
+ * @param {function} callback - The method it should call after the animation is
+ *  ended (in case animate is false then this won't be executed,
+ * it should be passed as part of the Parallel's or sequence's callback)
+ */
 export const useAnimateParallel = props =>
   useAnimateStructure({ ...props, animationType: 'parallel' });
 export const useAnimateSequence = props =>
