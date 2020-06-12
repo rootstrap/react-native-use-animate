3f64bf544675d1d8b3b860d4cd5c69ca62cf5685
fernandatoledo <fertoledo93@gmail.com>
2020-06-12 10:45:00 -0300

    Remove comment

--- demo/App.js
+++ demo/App.js
@@ -11,7 +11,6 @@ export default function App() {
     <View style={styles.container}>
       <ParallelAnimatedBox />
       <SequenceAnimatedBox />
-      {/* <ParallelSequenceAnimatedBox /> */}
     </View>
   );
 }
